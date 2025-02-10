3b3e8f10167c79220cb45aeb6722828830ebabc0
Robert Swanson-Neitzel <robertsn@hawaii.edu>
2025-02-10 06:54:16 +0000

    second attempt E26

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -9,7 +9,7 @@ function sumFor(list) {
     list.forEach(function (nums) {
         total += nums;
     });
-    console.log("sumFor total is ".concat(total));
+    console.log(total);
     return total;
 }
 function sumWhile(list) {
@@ -19,26 +19,21 @@ function sumWhile(list) {
         total += list[count];
         count++;
     }
-    console.log("sumWhile total is ".concat(total));
+    console.log(total);
     return total;
 }
 function sumRecursion(list, index, total) {
     if (index === void 0) { index = 0; }
     if (total === void 0) { total = 0; }
     if (index >= list.length) {
-        console.log("sumRecursion total is ".concat(total));
+        console.log(total);
         return total;
     }
     var newTotal = total + list[index];
     return sumRecursion(list, index + 1, newTotal);
 }
 function sumTheFunctionalWay(list) {
-    var theSum = list.reduce(function (total, numInList) { return total + numInList; }, 0);
-    console.log("sumTheFunctionalWay total is ".concat(theSum));
-    return theSum;
+    var newSum = list.reduce(function (numInList, total) { return numInList + total; }, 0);
+    console.log(newSum);
+    return newSum;
 }
-console.log(sumFor([1, 2, 3, 4]));
-console.log(sumWhile([1, 2, 3, 4]));
-console.log(sumRecursion([1, 2, 3, 4]));
-console.log(sumTheFunctionalWay([1, 2, 3, 4]));
-console.log('sumTheFunctionalWay: ', sumTheFunctionalWay([1, 2, 3, 4]));
--- sumofnumbers1.ts
+++ sumofnumbers1.ts
@@ -3,7 +3,7 @@ function sumFor(list: number[]): number {
   list.forEach((nums) => {
     total += nums;
   });
-  console.log(total);
+  console.log(`sumFor total is ${total}`);
   return total;
 }
 
@@ -14,25 +14,32 @@ function sumWhile(list: number[]): number {
     total += list[count];
     count++;
   }
-  console.log(total);
+  console.log(`sumWhile total is ${total}`);
   return total;
 }
 
 function sumRecursion(list: number[], index = 0, total = 0): number {
   if (index >= list.length) {
-    console.log(total);
+    console.log(`sumRecursion total is ${total}`);
     return total;
   }
+
   const newTotal = total + list[index];
+
   return sumRecursion(list, index + 1, newTotal);
 }
 
 function sumTheFunctionalWay(list: number[]): number {
-  const newSum = list.reduce((numInList, total) => numInList + total, 0);
-  console.log(newSum);
-  return newSum;
+  const theSum = list.reduce((total, numInList) => total + numInList, 0);
+  console.log(`sumTheFunctionalWay total is ${theSum}`);
+  return theSum;
 }
 
+console.log(sumFor([1, 2, 3, 4]));
+console.log(sumWhile([1, 2, 3, 4]));
+console.log(sumRecursion([1, 2, 3, 4]));
+console.log(sumTheFunctionalWay([1, 2, 3, 4]));
+console.log('sumTheFunctionalWay: ', sumTheFunctionalWay([1, 2, 3, 4]));
 export {
   sumFor, sumWhile, sumRecursion, sumTheFunctionalWay,
 };
